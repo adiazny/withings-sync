c028b0c64d6f588c1394b412d4d6a8569b2c49ab
Alan Diaz <adiazny@gmail.com>
2021-01-03 22:16:27 -0500

    Added initial getMeas with HTTP client exploration

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -14,14 +15,6 @@ type WithingsNotification struct {
 	Appli     int   `json:"appli"`
 	StartDate int64 `json:"startdate"`
 	EndDate   int64 `json:"enddate"`
-}
-
-func main() {
-	fmt.Println("Server Up")
-	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/weight", withingsNotificationHandler)
-	log.Fatal(http.ListenAndServe(":8090", nil))
-
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +33,7 @@ func withingsNotificationHandler(w http.ResponseWriter, r *http.Request) {
 	var notification WithingsNotification
 	err = json.Unmarshal(b, &notification)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, err.Error(), 500)
 	}
 
 	//Print Notification
@@ -57,4 +50,50 @@ func withingsNotificationHandler(w http.ResponseWriter, r *http.Request) {
 func convertToTime(unixTime int64) time.Time {
 	time := time.Unix(unixTime, 0)
 	return time
+}
+
+func getMeas(n WithingsNotification) (weight float64) {
+
+	//TODO: Continue testing HTTP client with GET Request
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "https://account.withings.com/oauth2_user/authorize2", nil)
+	if err != nil {
+		panic(err)
+	}
+	qParams := req.URL.Query()
+	qParams.Add("response_type", "code")
+	qParams.Add("client_id", "3ed034dcb34ebb1c9c2bb300242fca8cdcc3e92a63a1b73b90b7b19413937e60")
+	qParams.Add("state", "mystate")
+	qParams.Add("scope", "metrics")
+	qParams.Add("redirect_uri", "https://portfolio.alandiaz.com")
+	req.URL.RawQuery = qParams.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	weight = 1.0
+	return
+}
+
+func main() {
+	fmt.Println("Server Up")
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/weight", withingsNotificationHandler)
+	fmt.Println(getMeas(WithingsNotification{}))
+	log.Fatal(http.ListenAndServe(":8090", nil))
+
 }
