a8a1825deac130ac61c6dceea4e5dcd46c5f786d
Alan Diaz <adiazny@gmail.com>
2021-01-07 23:13:02 -0500

    Added MeasureResponse Struct to UnMarshall Getmeas Response. Updated ReadMe

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ An application to practice Go Lang: Sync's a user's weight from Withings API to 
 ## Withings API
 * https://developer.withings.com/
 
+### GetMeas formula
+Power of ten to multiply the value field to get the real value.
+Formula: value * 10^unit = real value.
+Eg: value = 20 and unit = -1 => real value = 2.
+
 ## Design
 
 ![](static/withings-sync.png)
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -17,6 +16,30 @@ type WithingsNotification struct {
 	Appli     int   `json:"appli"`
 	StartDate int64 `json:"startdate"`
 	EndDate   int64 `json:"enddate"`
+}
+
+type Measures struct {
+	Value int `json:"value"`
+	Type  int `json:"type"`
+	Unit  int `json:"unit"`
+}
+
+type MeasureGrps struct {
+	Grpid    int        `json:"grpid"`
+	Created  int64      `json:"created"`
+	Category int        `json:"category"`
+	Measures []Measures `json:"measures"`
+}
+
+type MeasureBody struct {
+	UpdateTime  int64         `json:"updatetime"`
+	TimeZone    string        `json:"timezone"`
+	MeasureGrps []MeasureGrps `json:"measuregrps"`
+}
+
+type MeasureResponse struct {
+	Status int         `json:"status"`
+	MB     MeasureBody `json:"body"`
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -62,7 +85,7 @@ func getMeas(n WithingsNotification) (weight float64) {
 		panic(err)
 	}
 	//TODO: Design how to call and add OAuth token
-	req.Header.Add("Authorization", "Bearer e43d13aedc108b06aa259fe3a4a2acaa70caedfa")
+	req.Header.Add("Authorization", "Bearer 0ff90340b36abcc09df998d1e287669b5f5c8b34")
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -70,24 +93,32 @@ func getMeas(n WithingsNotification) (weight float64) {
 	}
 	defer resp.Body.Close()
 
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Response status:", resp.Status)
 
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan() && i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+	//var jsonData interface{}
+	var meas MeasureResponse
+	json.Unmarshal(b, &meas)
+
+	fmt.Printf("Measurement %+v", meas)
+
 	weight = 1.0
 	return
 }
 
 func main() {
 	fmt.Println("Server Up")
+
 	http.HandleFunc("/about", aboutHandler)
+
 	http.HandleFunc("/weight", withingsNotificationHandler)
+
 	fmt.Println(getMeas(WithingsNotification{}))
+
 	log.Fatal(http.ListenAndServe(":8090", nil))
 
 }
